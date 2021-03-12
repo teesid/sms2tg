007405671b70f342e9f42b460845bcbc44365a64
teesid <teesid@instasim.com>
2021-03-12 12:07:06 +0700

    Removed config.py*.  Deleted the compromised TG bot.

--- config.py
+++
@@ -1,5 +0,0 @@
-TOKEN = '1663804340:AAEsRh1jnnB3Wka5XhdDimBGCv_ZtcKMJQM'
-BOT_API_URL = "https://api.telegram.org/bot"
-# To get the chat id
-# curl -s https://api.telegram.org/bot{token}/getUpdates
-CHAT_ID = '22042718'
